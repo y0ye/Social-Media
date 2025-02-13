31138b5366cb5878e15bbd81a181264dc49d7367
Bennett Oppenheimer <obennett66@gmail.com>
2025-02-12 22:23:03 -0600

    Add db file

--- server/database.sql
+++ server/database.sql
@@ -2,5 +2,6 @@ CREATE DATABASE posts;
 
 CREATE TABLE post(
     post_id SERIAL PRIMARY KEY,
+    title VARCHAR(144),
     description VARCHAR(255)
 );
--- server/index.js
+++ server/index.js
@@ -10,30 +10,84 @@ app.use(express.json());
 //ROUTES//
 
 //create a post
-
 app.post("/posts", async (req,res) => {
     try{
         const{description} = req.body;
-        const newPost = await pool.query("INSERT INTO post (description) VALUES($1)",
-            [description])
-        console.log(req.body);
+        const newPost = await pool.query(
+            "INSERT INTO post (description) VALUES($1) RETURNING *",
+            [description]
+        );
 
-        res.json(newPost);
+        res.json(newPost.rows[0]);
+    }
+    catch (err){
+        console.log(err.message);
+    }
+});
 
+//get all posts
+app.get("/posts", async (req,res) =>{
+    try{
+        const allPosts = await pool.query(
+            "SELECT * FROM post"
+        );
+
+        res.json(allPosts.rows);
     }
     catch (err){
         console.log(err.message);
     }
 })
 
-//get all posts
-
 //get a post
+app.get("/posts/:id", async (req,res) =>{
+    try{
+        const {id} = req.params;
+        const post = await pool.query(
+            "SELECT * FROM post WHERE post_id = $1",
+             [id]
+        );
+
+        res.json(post.rows[0]);
+    }
+    catch (err){
+        console.log(err.message);
+    }
+})
 
 //update a post
+app.put("/posts/:id", async (req, res) =>{
+    try{
+        const {id} = req.params;
+        const {description} = req.body;
+        const updateTodo = await pool.query(
+            "UPDATE post SET description = $1 WHERE post_id = $2",
+            [description, id]
+        );
+
+        res.json("Post updated!");
+    }
+    catch (err){
+        console.log(err.message);
+    }
+})
 
 //delete a post
+app.delete("/posts/:id", async (req, res) =>{
+    try{
+        const {id} = req.params;
+        const deletePost = await pool.query(
+            "DELETE FROM post WHERE post_id = $1",
+            [id]);
 
+        res.json("Post deleted!");
+    }
+    catch (err){
+        console.log(err.message);
+    }
+})
+
+//server listening
 app.listen(5000, () => {
     console.log("Server started");
 });
